fd68e5ae113692d20957778fc5648d533396f488
unknown <radowan.me@gmail.com>
2022-03-26 07:54:14 +0600

    first commit

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css'
+const Header = () => {
+    return (
+        <div className='header'>
+            <h1>The Perfume Shop</h1>
+            <h2>Choose 4 only</h2>
+        </div>
+    );
+};
+
+export default Header;
